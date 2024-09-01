2b62b9348b99b7d508547d052c29da0d3b307b67
Arundath2001 <arundatharun44@gmail.com>
2024-09-01 22:37:21 +0530

    latest commit

--- src/components/WorkData.jsx
+++ src/components/WorkData.jsx
@@ -11,6 +11,10 @@ import invv from "../assets/inv-video.webm";
 import b2bvv from "../assets/b2b-video.webm";
 import goodluckv from "../assets/goodluck-video.webm";
 import irctcv from "../assets/irctc-video.webm";
+import artiqv from "../assets/Artiq.webm";
+import bhoomikav from "../assets/Bhoomika.webm";
+import bhoomika from "../assets/bhoomika.jpeg";
+import artiq from "../assets/artiq.jpeg";
 
 function WorkData(){
 
@@ -25,6 +29,8 @@ function WorkData(){
             <WorkCard link="https://www.behance.net/gallery/191041807/Re-designed-Login-page-for-IRCTC-Rail-Connect-App" hoverEffect="hovereffect" text="Case Study" video={irctcv} url={irctc} head3="IRCTC Rail Connect Login Page " paratop="[Freelance Project]" paradown="Revolutionized the IRCTC Rail Connect app login page with a contemporary redesign, enhancing user experience and functionality." />
             <WorkCard  text="In-progress" video={goodluckv} url={goodluck} head3="Good Luck" paratop="[Freelance Project]" paradown="Your’s ultimate giveaway companion app where users can effortlessly create, join, and select winners for exciting giveaways." />
             <WorkCard  text="In-progress" url={terra} head3="Terra-One" paratop="[Company Project]" paradown="Your all-in-one warehouse management solution simplifies inventory tracking and logistics, ensuring smooth operations and optimal efficiency for your business." />
+            <WorkCard  text="In-progress" video={bhoomikav} url={bhoomika} head3="Real Estate Website Development" paratop="[Freelance Project]" paradown="Developed a comprehensive real estate platform designed to streamline property transactions. The website allows users to seamlessly list properties for sale, make inquiries, and book viewings." />
+            <WorkCard  text="In-progress" video={artiqv} url={artiq} head3="Artiq Design Studio Landing Page" paratop="[Freelance Project]" paradown="Created a visually captivating landing page for Artiq Design Studio, showcasing their range of design services." />
         </div>
 
     );
